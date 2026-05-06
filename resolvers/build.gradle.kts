2abdd56f3696622113e261ff027ec3ba0220131c
ViaBot <viabot@ductworks.io>
2026-05-06 12:18:58 -0600

    Update from airbnb/viaduct at release/v0.32.0
    GitOrigin-RevId: 0ab6d6a28fe6629b4e92951b1448906015abcf96

--- resolvers/build.gradle.kts
+++ resolvers/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `java-library`
     alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.ksp)
     alias(libs.plugins.viaduct.module)
 }
 
--- src/main/kotlin/com/example/viadapp/injector/ViaductConfiguration.kt
+++ src/main/kotlin/com/example/viadapp/injector/ViaductConfiguration.kt
@@ -4,18 +4,14 @@ import com.example.viadapp.SCHEMA_ID
 import viaduct.service.BasicViaductFactory
 import viaduct.service.SchemaRegistrationInfo
 import viaduct.service.SchemaScopeInfo
-import viaduct.service.TenantRegistrationInfo
 import viaduct.service.api.Viaduct
 
 object ViaductConfiguration {
     val viaductService: Viaduct by lazy {
-        BasicViaductFactory.create(
+        BasicViaductFactory.createFromResource(
             schemaRegistrationInfo = SchemaRegistrationInfo(
                 scopes = listOf(SchemaScopeInfo(SCHEMA_ID)),
             ),
-            tenantRegistrationInfo = TenantRegistrationInfo(
-                tenantPackagePrefix = "com.example.viadapp"
-            )
         )
     }
 }
